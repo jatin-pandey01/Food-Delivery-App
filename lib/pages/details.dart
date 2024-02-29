6a7951f9833c46d6b5769f2c9daf70c8af327385
jatin-pandey01 <=>
2024-02-29 23:10:03 +0530

    Login and Signup Done

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/widget/widget_support.dart';
 
 class Details extends StatefulWidget {
   const Details({super.key});
@@ -8,16 +11,111 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  int orderCount = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: 50.0,left: 10.0, right: 20.0),
-        child: GestureDetector(
-          onTap: (){
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.arrow_back_ios_new_outlined,color: Colors.black,)),
+        child: Column(
+          crossAxisAlignment: 
+          CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back_ios_new_outlined,color: Colors.black,)
+            ),
+            Image.asset("images/salad2.png", width: MediaQuery.of(context).size.width, height: MediaQuery.of(context).size.height/2.3, fit: BoxFit.fill, //BoxFit.fill means fill your body in give height and width
+            ),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                Text("Mix Veg Salad", style: AppWidget.HeadlineTextFieldStyle(),),
+                Spacer(), //Max space between 2 container possible will give
+                GestureDetector(
+                  onTap: (){
+                    if(orderCount > 1){
+                      orderCount -= 1; 
+                    }
+                    setState(() {
+                      
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10.0)),
+                    padding: EdgeInsets.all(2.0),
+                    child: Icon(Icons.remove,color: Colors.white,size: 25,),
+                  ),
+                ),
+                SizedBox(width: 25,),
+                Text(orderCount.toString(), style: AppWidget.BoldTextFieldStyle(),),
+                SizedBox(width: 25,),
+                GestureDetector(
+                  onTap: () {
+                    orderCount += 1;
+                    setState(() {
+                      
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10.0)),
+                    padding: EdgeInsets.all(2.0),
+                    child: Icon(Icons.add, color: Colors.white,size: 25,),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text("A vibrant mix veg salad bursts with colors and flavors, combining crisp lettuce, juicy tomatoes, crunchy cucumbers, and a medley of colorful bell peppers. Tossed with a zesty dressing, it's a refreshing and nutritious delight for the palate.", style: AppWidget.LightTextFieldStyle(),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Text("Delivery Time : ", style: AppWidget.SemiBoldTextFieldStyle(),),
+                SizedBox(width: 25.0,),
+                Icon(Icons.alarm, color: Colors.black,),
+                SizedBox(width: 5.0,),
+                Text("30 Min", style: AppWidget.SemiBoldTextFieldStyle(),),
+              ],
+            ),
+            Spacer(),
+            Container(
+              padding: EdgeInsets.only(bottom: 40.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Total Price : ", style: AppWidget.SemiBoldTextFieldStyle(),),
+                      Text("\$30", style: AppWidget.BoldTextFieldStyle(),),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(5.0)),
+                    padding: EdgeInsets.all(5.0),
+                    width: MediaQuery.of(context).size.width/2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text("Add to cart", style: TextStyle(color: Colors.white, fontSize: 18.0, fontFamily: 'Poppins'),),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[500], borderRadius: BorderRadius.circular(8)
+                          ),
+                          padding: EdgeInsets.all(3),
+                          child: Icon(Icons.shopping_cart_outlined,color: Colors.white,),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/signup.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/pages/login.dart';
+import 'package:flutter_application_1/widget/widget_support.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [Color(0xFFff5c30), Color(0xFFe74b1a)])),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/3),
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.only(topLeft: Radius.circular(25.0), topRight: Radius.circular(25.0))),
+              // child: Text(""),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/8,left: 20, right: 20),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Center(
+                      child: Image.asset("images/logo.png", width: MediaQuery.of(context).size.width/1.3,fit: BoxFit.cover,)
+                    ),
+                    Material(
+                      elevation: 10,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 20,right: 20),
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height/2,
+                        decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          children: [
+                            SizedBox(height: 30,),
+                            Text("Sign Up", style: AppWidget.HeadlineTextFieldStyle(),),
+                            
+                            SizedBox(height: 30,),
+                            TextField(
+                              style: AppWidget.SemiBoldTextFieldStyle(),
+                              decoration: InputDecoration(hintText: "Name", hintStyle: AppWidget.BoldTextFieldStyle(),prefixIcon: Icon(Icons.person_2_outlined)),
+                            ),
+                            SizedBox(height: 30,),
+                            TextField(
+                              style: AppWidget.SemiBoldTextFieldStyle(),
+                              decoration: InputDecoration(hintText: "Email", hintStyle: AppWidget.BoldTextFieldStyle(),prefixIcon: Icon(Icons.email_outlined)),
+                            ),
+                            SizedBox(height: 30,),
+                            TextField(
+                              style: AppWidget.SemiBoldTextFieldStyle(),
+                              obscureText: true,
+                              decoration: InputDecoration(hintText: "Password",hintStyle: AppWidget.BoldTextFieldStyle(), prefixIcon: Icon(Icons.password_outlined)),),
+                            SizedBox(height: 40,),
+                            Material(
+                              elevation: 5.0,
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
+                                width: 200,
+                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                decoration: BoxDecoration(color: Color(0xffff5722),//Color code always start with 0xff and after this that hex decimal code come
+                                borderRadius: BorderRadius.circular(20)),
+                                child: Center(child: Text("SIGN UP",style: TextStyle(color: Colors.white,fontSize: 18.0, fontFamily: 'Poppins', fontWeight: FontWeight.bold),)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 100,),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Login()));
+                        setState(() {
+                          
+                        });
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            child: Text("Already have an account ?", style: TextStyle(color: Colors.black, fontSize: 18.0,fontWeight: FontWeight.bold, fontFamily: 'Poppins' ),),
+                          ),
+                          Container(
+                            child: Text(" Login", style: TextStyle(color: Colors.red, fontSize: 20,decoration: TextDecoration.underline, decorationColor: Colors.red, decorationThickness: 2, fontWeight: FontWeight.w900, fontFamily: 'Poppins'),),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
